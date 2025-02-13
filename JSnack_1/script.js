28b552fdcc3100148541c16b37273a37db9c7e5d
imnxtamess <davide.morisco.work@gmail.com>
2025-02-13 10:45:01 +0100

    edit console log and add an else condition

--- JSnack_1/script.js
+++ JSnack_1/script.js
@@ -6,10 +6,15 @@ const number1 = Number(prompt("Insert the first number here: "))
 console.log(number1);
 
 const number2 = Number(prompt("Insert the second number here: "))
+console.log(number2);
 
 if (number1 > number2) {
-  console.log(number1);
+  console.log(`${number1} is greater than ${number2}`);
 }
 else if (number2 > number1) {
-  console.log(number2);
+  console.log(`${number2} is greater than ${number1}`);
+}
+else {
+  console.log("The numbers are equal");
+
 }
